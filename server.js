de6a8304bd7d124a251fa9c3245a7a03752158a1
rehat-teams <redhatteams@gmail.com>
2026-07-10 01:05:05 +0000

    MAJ-2

---
+++ server.js
@@ -0,0 +1,15 @@
+import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const app = express();
+const port = process.env.PORT || 8080;
+
+app.use(express.static(path.join(__dirname, 'dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+});
+
+app.listen(port, () => console.log(`Serveur lancé sur le port ${port}`));
