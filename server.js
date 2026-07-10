368aeea9545d3be4b271bc89a7ac8d851f83c88c
rehat-teams <redhatteams@gmail.com>
2026-07-10 01:55:51 +0000

    Corrige la route catch-all pour Express 5

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const port = process.env.PORT || 8080;
 
 app.use(express.static(path.join(__dirname, 'dist')));
 
-app.get('*', (req, res) => {
+app.use((req, res) => {
   res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
